82729e455d0612b7eb96dd07d1a725a70b82ace4
Mahesh324-Babu <bandikarbabu3324@gmail.com>
2026-04-16 21:57:27 +0530

    Delete backend directory

--- backend/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from prompts import views
-
-urlpatterns = [
-    path('prompts/', views.get_prompts),
-    path('prompts/', views.create_prompt),
-    path('prompts/<int:id>/', views.get_prompt_detail),
-]
